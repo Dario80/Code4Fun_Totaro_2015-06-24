9d509d53589dad5eec9fffb87004365f4593a47f
Dario80 <totaro.dario@gmail.com>
2015-06-25 12:33:19 +0200

    Commit seconda fase (Green)  nel flusso TDD con aggiunta progetto ArrayIntersection  + file problems.txt contenente i problemi da svolgere + framework utilizzati

---
+++ Exercises.UnitTests/ArrayIntersection/ArrayIntersection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOperations
+{
+    public interface IIntersection
+    {
+      int[]  GetArrayIntersection(int[] firstArray, int[] secondArray);
+    }
+
+    public class Intersection : IIntersection
+    {
+
+        public int[] GetArrayIntersection(int[] firstArray, int[] secondArray)
+        {
+            return new int[0];
+        }
+    }
+}
