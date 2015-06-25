912523ccb17c131bb3e3416e98b946e80f52564c
Dario80 <totaro.dario@gmail.com>
2015-06-25 11:45:23 +0200

    aa

---
+++ Exercises.UnitTests/Exercises.UnitTests/ArrayIntersectionTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOperations.UnitTests
+{
+    [TestFixture]
+    public class ArrayIntersectionTests
+    {
+        private IIntersection _arrayIntersection;
+        private int[] _firstArray;
+        private int[] _secondArray;
+
+        [SetUp]
+        public void SetupUnitTests()
+        {
+            _arrayIntersection = new Intersection();
+            _firstArray = new int[0];
+            _secondArray = new int[0];
+
+        }
+        [Test]
+        public void NeitherOfTwoArrayHaveNumber()
+        {
+            int[] expected =  new int[0];
+            var actual = _arrayIntersection.GetArrayIntersection(_firstArray, _secondArray);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
