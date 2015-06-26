615d393ddd7e9e871144a497894c58bf84b30fb8
Dario80 <totaro.dario@gmail.com>
2015-06-26 16:28:08 +0200

    Prima fase (Red) secondo esercizio sulle liste lincate generiche

---
+++ Exercises.UnitTests/GenericSinglyLynkedListOperations/LinkedListOperationsTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListUtilities.UnitTests
+{
+    [TestFixture]
+    public class LinkedListOperationsTests
+    {
+        private ILinkedListOperations<int> _linkedListOperations;
+        private int _index;
+        private SinglyLinkedList<int> _singlyLinkedList;
+
+        [SetUp]
+        public void SetupUnitTests()
+        {
+            _linkedListOperations = new LinkedListOperations<int>();
+            _singlyLinkedList = new SinglyLinkedList<int>();
+        }
+
+
+        [Test]
+        public void GenericSinglyLinkedListIsEmpty()
+        {
+            _index = 5;
+            int expected = 0;
+            var actual = _linkedListOperations.GetNthElementFromLast(_singlyLinkedList, _index);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
